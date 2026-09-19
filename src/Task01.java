3a2f1962b846c01429dfcf4ca09cea81f4164662
nahomair716 <nahomair716@gmail.com>
2026-09-19 11:21:52 -0400

    Update Task01.java

--- src/Task01.java
+++ src/Task01.java
@@ -1,2 +1,13 @@
-public class Task01 {
+public class Task04 {
+    public void main(){
+        int numToExamine = 63;
+
+        if (numToExamine % 2 ==0){
+            System.out.println("The number "+ numToExamine+ " is even!");
+        }else{
+            System.out.println("The number "+ numToExamine+ " is odd!");
+        }
+    }
 }
+// remember to zip it
+// to save
